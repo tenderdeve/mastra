5faa66167917eb3f1ced832f0668385245e3f1fd
Eric Pinzur <epinzur@gmail.com>
2026-05-05 17:51:30 +0200

    added core feature gate

--- packages/core/src/features/index.ts
+++ packages/core/src/features/index.ts
@@ -20,6 +20,6 @@ export const coreFeatures = new Set<string>([
   'request-response-id-rotation',
   'workspaces-v1',
   'datasets',
-  'observability:v1.13.2',
+  'observability:v1.32.0',
   'channels',
 ]);
